78525a8ad5ee29c6e2aa7065acdb9d6763b9c794
Rita Liu <milkhome0430@gmail.com>
2024-07-16 12:44:15 +0800

    namespace介紹

--- namespace.py
+++ namespace.py
@@ -1,3 +1,15 @@
-x = 5 
-def f():
-    x = 5
+# LEGB
+# L: local    最小
+# E: enclosed 包夾在function中間
+# G: global   全域
+# b: built-in 內建
+
+y = 10      #global
+x = 5       #global
+def f():     #global
+    x = 1   #enclosed
+    def ff():
+        x = 5   #local
+
+def qwe():  #global
+    q = 1   #local
