4775eb7d457676ccffa393159c8dc8b584a0ae99
Rita Liu <milkhome0430@gmail.com>
2024-07-18 11:35:31 +0800

    註解規則跟string Formatting的使用/usr/local/bin/python3 /Users/ritaliu/vscode/pythonLearning_2/string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,30 @@
+x = 5  # 程式後面空兩格，#號後面空一格 
+
+# docstring  # doc documentation  介紹module 或是 class 的註解（說明書）
+
+class Batman:
+
+    """_summary_
+    kj;fafl;j
+    fkaj;f
+
+    """
+
+
+# string Formatting
+    
+# pringf 舊型
+name = 'Allen'
+x = 5
+y = 3.14
+# print('hi %s' % name) # 字串型用s   # hi Allen
+# print('hi %d' % x) # 整數用d   # hi 5
+# print('hi %f' % y) # 小數用d   # hi 3.140000
+# print('hi %s, my number is %d' % (name, x) ) # 組合 # hi Allen, my number is 5
+
+
+# 新型
+# print('hi {}, my number is {}'.format(name, x))
+
+#python3.6之後才有的
+print(f'hi {name}, my number is {x}')
